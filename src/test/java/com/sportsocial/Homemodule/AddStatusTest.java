28e389b3863ad69dd7cbd66fa35b763a1e02d4e5
USER <USER@DESKTOP-HGOMU4C>
2022-12-19 11:42:52 +0530

    test script updated

--- src/test/java/com/sportsocial/Homemodule/AddStatusTest.java
+++ src/test/java/com/sportsocial/Homemodule/AddStatusTest.java
@@ -1,16 +1,31 @@
 package com.sportsocial.Homemodule;
 
+import java.io.File;
+
 import org.testng.annotations.Test;
 
 import com.sportsocial.POM.Uploadstatus;
 import com.sportsocial.genaricUtility.BaseClassAdmin;
+import com.sportsocial.genaricUtility.FilePaths;
 
 public class AddStatusTest extends BaseClassAdmin {
+	Uploadstatus uploadstatus;
+	String expectedText;
+	String expectedImage;
+	File file;
 	
 	@Test
-	public void addStatusWithPhoto() {
-		Uploadstatus uploadstatus= new Uploadstatus(driver);
-		uploadstatus.shareThepost(null, null);
+	public void addStatusWithPhoto() throws InterruptedException {
+		uploadstatus= new Uploadstatus(driver);
+		expectedText = excelfile.getExcelData(FilePaths.HOME_MODULE_SHEET, 10, 0)+javautil.getradomNo(100);
+		expectedImage = excelfile.getExcelData(FilePaths.HOME_MODULE_SHEET, 17, 1);
+		file=new File(expectedImage);
+		String expImage = file.getAbsolutePath();
+				
+		System.out.println(expectedText);
+		System.out.println(expImage);
+		uploadstatus.shareThepost(expectedText, expImage);
+		
 		
 	}
 
